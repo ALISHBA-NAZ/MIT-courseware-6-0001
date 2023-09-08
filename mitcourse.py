80a907c5abf5eb6c90c6cc2e0828ec4de6746e56
ALISHBA-NAZ <143730027+ALISHBA-NAZ@users.noreply.github.com>
2023-09-08 18:14:10 +0500

    Add files via upload

---
+++ mitcourse.py
@@ -0,0 +1,104 @@
+ # strings
+#hi = "hello dear"
+#name = "alish"
+#greet = hi + name
+#print(greet)
+#greeting = hi + " " + name
+#print(greeting)
+#silly = hi + (" " + name)*3
+#print(silly)
+
+#once = "umbr"
+#repeat = "ella"
+#u = once +(repeat+ " " )*4
+#print(u)
+
+
+ # output
+#x = 1
+#print(x)
+#x_str = str(x)
+#print("my fav number is", x, ".", "x=", x)
+#print("my fav number is", x_str + ".", "x=" + x_str)
+#print("my fav number is" + x_str + "." + "x=" + x_str)
+
+# input
+#text = input("type anything..")
+#print(5*text)
+#num = int(input("type a number.."))
+#15print(5*num)
+
+# conditionals/branching
+#x = float(input("enter any number for x: ")) 
+#y = float(input("enter any number for y: "))
+#if x == y:
+ #   print("x and y are equal")
+  #  if y != y:
+   #     print("therefore, x/y is", x/y)
+#elif x < y:
+ #   print("x is smaller") 
+#else:
+ #   print("y is smaller")    
+
+# remainder 
+#num = int(input("enter a number: "))
+#if num % 2 == 0:
+  #  print(" number is even") 
+#else:
+ #   print("number is odd")  
+
+# while loops
+#n = input("You are in the Lost Forest\n****************\n****************\n :)\n****************\n****************\nGo left or right? ")
+#while n == "right" or n == "Right":
+ #   n = input("You are in the Lost Forest\n****************\n******       ***\n  (hey dear\n****************\n****************\nGo left or right? ")
+#print("\nYou got out of the Lost Forest!\n\o/")
+
+#n = 0
+#while n < 5:
+#    print(n)
+ #   n = n + 1
+
+# for loops
+#for n in range(5):
+ #   print(n) 
+
+#for n in range(10):
+ #   print(n)    
+
+#mysum = 0
+#for i in range(5,8):
+ #   mysum += 1
+#print(mysum)
+
+#mysum = 0
+#for i in range (5,11,2):
+ #   mysum += i
+  #  if mysum == 5:
+   #     break
+   # mysum += 1
+#print(mysum)
+
+# perfect squares
+#ans = 0
+#neg_flag = False
+#x = int(input("Enter an integer: "))
+#if x < 0:
+#    neg_flag = True
+#while ans**2 < x:
+#    ans = ans + 1
+#if ans**2 == x:
+#    print("Square root of", x, "is", ans)
+#else:
+#    print(x, "is not a perfect square")
+#    if neg_flag:
+#        print("Just checking... did you mean", -x, "?")
+
+# comparision 
+pset_time = 15
+sleep_time = 8
+print(sleep_time > pset_time)
+derive = False
+drink = True
+both = derive and drink
+print(both)
+
